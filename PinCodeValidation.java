7ecf8e0fabfa405277759baf767758d045ef9063
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-29 12:01:17 +0530

    [Gaurav]REFACTORED: Validate number of digits in PIN code

--- PinCodeValidation.java
+++ PinCodeValidation.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class PinCodeValidation 
 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to PIN Code Validation Program");
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the PIN Code: ");
+		String pincode = scan.next();
+		
+		Pattern pattern = Pattern.compile("\\d{6}");
+		Matcher match = pattern.matcher(pincode);
+		
+		if(match.matches())
+		{
+			System.out.println("Valid PIN Code");
+		}
+		else
+		{
+			System.out.println("Invalid PIN Code");
+		}
 	}
 }
